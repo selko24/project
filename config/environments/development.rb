02f6f26ebdbbdddcd794e88fbd519d6687d85ba4
denis selko <denis.selic24@gmail.com>
2012-01-28 22:05:15 +0100

    krneki

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Digg::Application.configure do
+Project::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Digg::Application.configure do
+Project::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
